cfd8107c9566ac1d4a2f04be4f7ecdcfbf964f3b
Samuel Bernard <samuel.bernard@s4m.io>
2015-09-08 16:39:21 +0200

    Install and configure bind
    - Install bind from package
    - Configure named.conf according to attributes
    - Set default attributes from default config on centos 7

---
+++ attributes/default.rb
@@ -0,0 +1,63 @@
+#
+# Copyright (c) 2015 Sam4Mobile
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default['bind-ddns']['package'] = 'bind'
+default['bind-ddns']['config_dir'] = '/etc'
+
+default['bind-ddns']['options'] = {
+  'listen-on port' => '53 { 127.0.0.1; }',
+  'listen-on-v6 port' => '53 { ::1; }',
+  'directory' => '"/var/named"',
+  'dump-file' => '"/var/named/data/cache_dump.db"',
+  'statistics-file' => '"/var/named/data/named_stats.txt"',
+  'memstatistics-file' => '"/var/named/data/named_mem_stats.txt"',
+  'allow-query' => '{ localhost; }',
+  'recursion' => 'yes',
+  'dnssec-enable' => 'yes',
+  'dnssec-validation' => 'yes',
+  'dnssec-lookaside' => 'auto',
+  'bindkeys-file' => '"/etc/named.iscdlv.key"',
+  'managed-keys-directory' => '"/var/named/dynamic"',
+  'pid-file' => '"/run/named/named.pid"',
+  'session-keyfile' => '"/run/named/session.key"'
+}
+
+default['bind-ddns']['channels'] = [
+  {
+    'name' => 'default_debug',
+    'config' => {
+      'file' => '"data/named.run"',
+      'severity' => 'dynamic'
+    }
+  }
+]
+
+default['bind-ddns']['categories'] = []
+
+default['bind-ddns']['zones'] = [
+  {
+    'name' => '"." IN',
+    'config' => {
+      'type' => 'hint',
+      'file' => '"named.ca"'
+    }
+  }
+]
+
+default['bind-ddns']['included_files'] = [
+  'named.rfc1912.zones',
+  'named.root.key'
+]
---
+++ recipes/config.rb
@@ -0,0 +1,31 @@
+#
+# Copyright (c) 2015 Sam4Mobile
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+conf_vars = {
+  'options' => node['bind-ddns']['options'],
+  'channels' => node['bind-ddns']['channels'],
+  'categories' => node['bind-ddns']['categories'],
+  'zones' => node['bind-ddns']['zones'],
+  'included_files' => node['bind-ddns']['included_files'],
+  'config_dir' => node['bind-ddns']['config_dir']
+}
+
+# Write configuration
+template "/#{node['bind-ddns']['config_dir']}/named.conf" do
+  source "named.conf.erb"
+  mode '644'
+  variables conf_vars
+ end
