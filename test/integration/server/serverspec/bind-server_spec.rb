59dd45598d2ae88f30d0fc011a242fe363b0af36
Samuel Bernard <samuel.bernard@s4m.io>
2015-09-16 15:02:41 +0200

    Add serverspec tests for bind server

---
+++ test/integration/server/serverspec/bind-server_spec.rb
@@ -0,0 +1,79 @@
+#
+# Copyright (c) 2015 Sam4Mobile
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+
+describe service('named') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(53) do
+  it { should be_listening.with('tcp') }
+end
+
+describe host('ns.kitchen') do
+  it { should be_resolvable.by('dns') }
+  its(:ipv4_address) { should eq get_ip_eth0 }
+end
+
+describe host('server-ddns') do
+  it { should be_resolvable.by('dns') }
+  its(:ipv4_address) { should eq get_ip_eth0 }
+end
+
+describe file('/etc/resolv.conf') do
+  its(:content) { should eq <<-eos.gsub(/^ {4}/, '') }
+    # Produced by Chef -- changes will be overwritten
+
+    search kitchen
+    nameserver 127.0.0.1
+  eos
+end
+
+describe file('/etc/named.conf') do
+  its(:content) { should contain 'listen-on port 53 { localnets; };' }
+  its(:content) { should contain 'allow-query { localnets; };' }
+  its(:content) { should contain 'recursion yes;' }
+  its(:content) { should contain 'zone kitchen {' }
+  its(:content) { should contain 'file "dynamic/db-kitchen";' }
+  its(:content) { should contain 'include "/etc/named-kitchen.key";' }
+end
+
+describe file('/etc/named-kitchen.key') do
+  its(:content) { should eq <<-eos.gsub(/^ {4}/, '') }
+    // Produced by Chef -- changes will be overwritten
+
+    key kitchen {
+      algorithm HMAC-MD5;
+      secret "9ZDQZxLEBuho4+O0EuGOYA==";
+    };
+  eos
+end
+
+describe file('/var/named/dynamic/db-kitchen.erb') do
+  its(:content) { should contain '@ IN SOA ns.kitchen. root@kitchen. \(' }
+  its(:content) { should contain '  IN NS ns.kitchen.' }
+  its(:content) { should contain "ns.kitchen. IN A #{get_ip_eth0}" }
+end
+
+describe command('named-checkconf -z /etc/named.conf') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('named-checkzone kitchen /var/named/dynamic/db-kitchen') do
+  its(:exit_status) { should eq 0 }
+end
