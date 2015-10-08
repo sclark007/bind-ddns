c200d600d38ce4fe367b391c0ef1c68e43aa6a48
Samuel Bernard <samuel.bernard@s4m.io>
2015-10-07 20:11:22 -0400

    Update short description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'bind-ddns'
 maintainer 'Sam4Mobile'
 maintainer_email 'dps.team@s4m.io'
 license 'Apache 2.0'
-description 'Wrapper around ddnsupdate cookbook'
+description 'Install and configure ISC Bind on server and nsupdate on clients'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://gitlab.com/s4m-chef-repositories/bind-ddns'
 issues_url 'https://gitlab.com/s4m-chef-repositories/bind-ddns/issues'
